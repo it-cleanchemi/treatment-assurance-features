e2aeac616da758697799f44bca8b40d1364f701a
Vladimir Martysevich <v.martysevich@cleanchemi.com>
2023-08-03 14:10:46 -0500

    test

--- GAS/Code.js
+++ GAS/Code.js
@@ -1,4 +1,4 @@
-//Revision 8/03/2023 - with emailing stage report
+//Revision 8/03/2023 - with emailing stage report -- 
 
 var TA = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Treatment Assurance Reporting');
 var activeCell = TA.getActiveCell(); //TA.getRange("B84");
